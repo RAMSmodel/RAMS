7ad632768ea021f0dc028dd090b56a7e3956f9ce
smsaleeb <smsaleeb@gmail.com>
2026-08-31 15:21:15 -0600

    A few updates for new software paths and test simulations.

--- bin.rams/cleanup.sc
+++ bin.rams/cleanup.sc
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm -f *.mod rams rams*.a
-rm -fr testrun.output
+rm -fr testsupercell.output
--- include.mk
+++ include.mk
@@ -29,13 +29,13 @@ RAMS_VERSION=6.3.04
 # Typically can use "parallel" for either, but some supercomputers require
 # use of the serial executable.
 #############################################################################
-HDF5_ROOT=/home/smsaleeb/software/hdf5-1.10.7
+HDF5_ROOT=/home/smsaleeb/software/hdf5-1.14.3
 
 #############################################################################
 # Set root locations for parallel processing MPI software.
 # You can comment out MPI_ROOT for serial processing compile.
 #############################################################################
-MPI_ROOT=/home/smsaleeb/software/mpich-3.3.2
+MPI_ROOT=/home/smsaleeb/software/mpich-4.3.2
 
 #############################################################################
 # Do not change these 2. They point from RAMS_ROOT to the source code.
@@ -76,6 +76,7 @@ CMACH=PC_LINUX1  #Standard Linux (only option available now)
 # files. The F_OPTS1 and F_OPTS2 variables below can hold different compiler
 # flags as needed. Further, for duplication of results you may need to force
 # IEEE standard which is done in examples below.
+# To debug do something like: "ifx -g -traceback -check all -fpe0"
 # Please coordinate variables below with the commands in the Makefile.
 # Definitions of compiler flags below
 # F_COMP      = path to compiler executable
@@ -92,8 +93,7 @@ CMACH=PC_LINUX1  #Standard Linux (only option available now)
 # (-g) for debugging, (-traceback) for more compiler error info
 # (-check bounds) for array bounds checking, (-fp-model precise) for IEEE
 # (-check uninit) for finding uninitialized variables, (-free) for free format
-#F_COMP=/home/smsaleeb/intel/composer_xe_2011_sp1.8.273/bin/intel64/ifort
-F_COMP=/home/smsaleeb/software/mpich-3.3.2/bin/mpif90
+F_COMP=/home/smsaleeb/software/mpich-4.3.2/bin/mpif90
 F_OPTS1=-free -O1 -fp-model precise -diag-disable=10448
 F_OPTS2=-free -O2 -fp-model precise -diag-disable=10448
 LOADER_OPTS= -free -O2 -fp-model precise
@@ -160,8 +160,7 @@ LIBS=
 # that are not really as issue for us, but you can turn warnings back on by
 # removing the "-w" if you wish to alter code to eliminate warnings.
 #############################################################################
-#C_COMP=gcc
-C_COMP=/home/smsaleeb/software/mpich-3.3.2/bin/mpicc
+C_COMP=/home/smsaleeb/software/mpich-4.3.2/bin/mpicc
 C_OPTS=-O3 -DUNDERSCORE -DLITTLE -std=gnu99 -DENABLE_PARALLEL_COMPRESSION -w
 #C_OPTS=-O3 -DUNDERSCORE -DLITTLE -std=gnu99 -DRAMS_DOUBLE_PREC \
 #  -DENABLE_PARALLEL_COMPRESSION -w
